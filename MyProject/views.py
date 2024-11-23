59c8eebafa36c370e1acec26ecde20b47f6d1528
kushal3008 <131846101+kushal3008@users.noreply.github.com>
2024-11-24 00:46:30 +0530

    ad

---
+++ MyProject/views.py
@@ -0,0 +1,8 @@
+#from django.http import HttpResponse
+from django.shortcuts import render
+def homepage(request):
+    #return HttpResponse("Hello World.")
+    return render(request,'home.html')
+def about(request):
+    #return HttpResponse("This is my about page.")
+    return render(request,'about.html')
